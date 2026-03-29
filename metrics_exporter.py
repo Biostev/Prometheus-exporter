c73175e6cef5f701473afed01f1e957e92db8768
Biostev <konakov06@mail.ru>
2026-03-29 20:20:07 +0300

    gitignore: only needed fields left
    requirements.txt created using pipreqs
    
    metrics_exporter.py:
    all asked metrics implemented
    
    simple_switch_with_metrics.py:
    all metrics except ports and flows work

--- metrics_exporter.py
+++ metrics_exporter.py
@@ -1,28 +1,138 @@
 import threading
 import http.server
-from prometheus_client import Counter, Gauge, generate_latest, REGISTRY
-
+from prometheus_client import (
+    Counter, Gauge, Histogram,
+    generate_latest, REGISTRY
+)
 
 class MetricsExporter:
     def __init__(self, port=8000):
         self.port = port
 
+        # Connections
+        # Number of active connections with switches
         self.active_connections = Gauge(
             'osken_active_connections',
             'Number of active datapath connections',
             ['switch_id']
         )
 
+        # OpenFlow msgs
+        # Number of PacketIn
+        self.packet_in_count = Counter(
+            'osken_packet_in_total',
+            'Total number of PacketIn messages received',
+            ['switch_id']
+        )
+
+        # Number of PacketOut
+        self.packet_out_count = Counter(
+            'osken_packet_out_total',
+            'Total number of PacketOut messages sent',
+            ['switch_id']
+        )
+
+        # Number of FlowMod
+        self.flow_mod_count = Counter(
+            'osken_flow_mod_total',
+            'Total number of FlowMod messages sent',
+            ['switch_id']
+        )
+
+        # Number of StatsRequest
         self.stats_request_count = Counter(
             'osken_stats_request_total',
             'Total number of StatsRequest messages',
-            ['switch_id', 'stats_type']
+            ['switch_id']
         )
 
+        # Number of StatsReply
         self.stats_reply_count = Counter(
             'osken_stats_reply_total',
             'Total number of StatsReply messages',
-            ['switch_id', 'stats_type']
+            ['switch_id']
+        )
+
+        # Processing latency
+        # PacketIn processing time
+        self.packet_in_processing_time = Histogram(
+            'osken_packet_in_processing_seconds',
+            'Time to process a PacketIn message',
+            ['switch_id'],
+            buckets=(0.01, 0.05, 0.1, 0.5, 1, 2.5, 5, 10)
+        )
+
+        # Errors
+        # Number of OpenFlow error messages
+        self.error_msg_count = Counter(
+            'osken_error_messages_total',
+            'Total number of OpenFlow error messages',
+            ['switch_id']
+        )
+
+        # Flows
+        # Number of flows
+        self.flows_count = Gauge(
+            'osken_flows_total',
+            'Total number of flows in flow tables',
+            ['switch_id', 'table_id']
+        )
+
+        # Number of deleted/expired flows
+        self.expired_flows_count = Counter(
+            'osken_expired_flows_total',
+            'Total number of expired or removed flows',
+            ['switch_id']
+        )
+
+        # Max table size
+        self.max_table_size = Gauge(
+            'osken_table_max_size',
+            'Max number of flows for table',
+            ['switch_id', 'table_id']
+        )
+
+        # Current table size
+        self.cur_table_size = Gauge(
+            'osken_table_current_size',
+            'Current number of flows in table',
+            ['switch_id', 'table_id']
+        )
+
+        # Ports
+        # Current port status
+        self.purt_status = Gauge(
+            'osken_port_status',
+            'Port status (1 = up, 0 = down)',
+            ['switch_id', 'port_name']
+        )
+
+        # Number of RX packets for port
+        self.rx_count = Counter(
+            'osken_port_rx_total',
+            'Total number recieved packets for port',
+            ['switch_id', 'port_name']
+        )
+
+        # Number of TX packets for port
+        self.tx_count = Counter(
+            'osken_port_tx_total',
+            'Total number transmitted packets for port',
+            ['switch_id', 'port_name']
+        )
+
+        # Number of RX errors/collisions/drops
+        self.rx_errors = Counter(
+            'osken_port_rx_errors',
+            'Total number of recieved errors for port',
+            ['switch_id', 'port_name']
+        )
+
+        # Number of TX errors/collisions/drops
+        self.tx_errors = Counter(
+            'osken_port_tx_errors',
+            'Total number of transmitted errors for port',
+            ['switch_id', 'port_name']
         )
 
         self._start_http_server()
--- simple_switch_with_metrics.py
+++ simple_switch_with_metrics.py
@@ -6,6 +6,9 @@ from os_ken.ofproto import ofproto_v1_3
 from os_ken.lib.packet import packet
 from os_ken.lib.packet import ethernet
 from os_ken.lib.packet import ether_types
+from os_ken.lib import hub
+
+import time
 
 from metrics_exporter import MetricsExporter
 
@@ -25,6 +28,19 @@ class SimpleSwitch13(app_manager.OSKenApp):
 
         self.logger.info("SimpleSwitch13 with Prometheus metrics started")
         self.logger.info("Metrics available at http://localhost:8000/metrics")
+        
+        self.start_timer()
+    
+    def start_timer(self):
+        hub.spawn(self._send_periodic_stats)
+    
+    def _send_periodic_stats(self):
+        while True:
+            hub.sleep(10)
+            for dpid, datapath in self.datapaths.items():
+                if datapath.id in self.datapaths.items():
+                    self.send_stats_request(datapath, 'flow')
+                    self.send_stats_request(datapath, 'table')
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -38,39 +54,82 @@ class SimpleSwitch13(app_manager.OSKenApp):
 
         # Update active connections metric
         self.metrics.active_connections.labels(switch_id=str(dpid)).set(1)
-        self.logger.info("Switch %s connected", dpid)
+        self.logger.info(f"Switch {dpid} connected")
 
         # install table-miss flow entry
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
+        actions = [parser.OFPActionOutput(
+            ofproto.OFPP_CONTROLLER,
+            ofproto.OFPCML_NO_BUFFER,
+        )]
         self.add_flow(datapath, 0, match, actions)
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
-                                             actions)]
+        dpid = str(datapath.id)
+
+        # Increase FlowMod counter
+        self.metrics.flow_mod_count.labels(switch_id=dpid).inc()
+
+        inst = [parser.OFPInstructionActions(
+            ofproto.OFPIT_APPLY_ACTIONS,
+            actions,
+        )]
         if buffer_id:
-            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
-                                    instructions=inst)
+            mod = parser.OFPFlowMod(
+                datapath=datapath,
+                buffer_id=buffer_id,
+                match=match,
+                instructions=inst,
+            )
         else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
-                                    match=match, instructions=inst)
+            mod = parser.OFPFlowMod(
+                datapath=datapath,
+                priority=priority,
+                match=match,
+                instructions=inst,
+            )
         datapath.send_msg(mod)
+    
+    def send_packet_out(self, datapath, buffer_id, in_port, actions, data=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        dpid = str(datapath.id)
+
+        # Increase PacketOut counter
+        self.metrics.packet_out_count.labels(switch_id=dpid).inc()
+
+        msg = parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=data,
+        )
+        datapath.send_msg(msg)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
+        start_time = time.perf_counter()
+
         if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("packet truncated: only %s of %s bytes",
-                              ev.msg.msg_len, ev.msg.total_len)
+            self.logger.debug(
+                f"packet truncated: only {ev.msg.msg_len} \
+                    of {ev.msg.total_len} bytes",
+            )
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
+
+        dpid = str(datapath.id)
+
+        # Increase PacketIn counter
+        self.metrics.packet_in_count.labels(switch_id=dpid).inc()
 
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
@@ -81,10 +140,9 @@ class SimpleSwitch13(app_manager.OSKenApp):
         dst = eth.dst
         src = eth.src
 
-        dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        self.logger.info(f"packet in {dpid} {str} {dst} {in_port}")
 
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
@@ -101,6 +159,9 @@ class SimpleSwitch13(app_manager.OSKenApp):
             match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
             if msg.buffer_id != ofproto.OFP_NO_BUFFER:
                 self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                # Record processing time
+                processing_time = time.perf_counter() - start_time
+                self.metrics.packet_in_processing_time.labels(switch_id=dpid).observe(processing_time)
                 return
             else:
                 self.add_flow(datapath, 1, match, actions)
@@ -108,7 +169,79 @@ class SimpleSwitch13(app_manager.OSKenApp):
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
+        
+        self.send_packet_out(datapath, msg.buffer_id, in_port, actions, data)
 
-        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                  in_port=in_port, actions=actions, data=data)
-        datapath.send_msg(out)
+        # Record processing time
+        processing_time = time.perf_counter() - start_time
+        self.metrics.packet_in_processing_time.labels(switch_id=dpid).observe(processing_time)
+
+    @set_ev_cls(ofp_event.EventOFPErrorMsg, MAIN_DISPATCHER)
+    def error_msg_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        dpid = str(datapath.id)
+        
+        # Increment error counter
+        self.metrics.error_msg_count.labels(switch_id=dpid).inc()
+        
+        self.logger.warning(f"OpenFlow error: switch={dpid} type={msg.type}")
+    
+    @set_ev_cls(ofp_event.EventOFPFlowRemoved, MAIN_DISPATCHER)
+    def flow_removed_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        dpid = str(datapath.id)
+        
+        # Increment expired flows counter
+        self.metrics.expired_flows_count.labels(switch_id=dpid).inc()
+        
+        self.logger.debug(f"Flow removed: switch={dpid} reason={msg.reason}")
+    
+    @set_ev_cls(ofp_event.EventOFPStatsReply, MAIN_DISPATCHER)
+    def stats_reply_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        dpid = str(datapath.id)
+
+        # Increment StatsReply counter
+        self.metrics.stats_reply_count.labels(switch_id=dpid).inc()
+    
+        for stat in msg.body:
+            port_no = stat.port_no
+            port_name = f"port_{port_no}"
+            
+            self.metrics.rx_count.labels(
+                switch_id=dpid, port_name=port_name
+            ).inc(stat.rx_packets)
+            
+            self.metrics.tx_count.labels(
+                switch_id=dpid, port_name=port_name
+            ).inc(stat.tx_packets)
+            
+            rx_errors = stat.rx_errors + stat.rx_dropped + stat.rx_over_errors + stat.rx_crc_errors
+            tx_errors = stat.tx_errors + stat.tx_dropped
+            
+            if rx_errors > 0:
+                self.metrics.rx_errors.labels(
+                    switch_id=dpid, port_name=port_name
+                ).inc(rx_errors)
+            
+            if tx_errors > 0:
+                self.metrics.tx_errors.labels(
+                    switch_id=dpid, port_name=port_name
+                ).inc(tx_errors)
+    
+    def send_stats_request(self, datapath, stats_type='flow'):
+        parser = datapath.ofproto_parser
+        dpid = str(datapath.id)
+        
+        # Increment StatsRequest counter
+        self.metrics.stats_request_count.labels(switch_id=dpid).inc()
+        
+        if stats_type == 'flow':
+            req = parser.OFPFlowStatsRequest(datapath)
+            datapath.send_msg(req)
+        elif stats_type == 'table':
+            req = parser.OFPTableStatsRequest(datapath)
+            datapath.send_msg(req)
